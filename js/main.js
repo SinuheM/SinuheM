7a8bc1963e4f857f5498f9e1de266b37b9005f3f
Adrian Priale C <adrian.priale@gmail.com>
2019-06-06 20:44:04 -0500

    Update works, use mustache to get data from json

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-$(document).ready(function(){$("a").on("click",function(o){if(""!==this.hash){o.preventDefault();var n=this.hash;$("html, body").animate({scrollTop:$(n).offset().top},800,function(){window.location.hash=n})}}),$(".infinite-toggle").on("mouseenter",function(){$(this).children().removeClass("animated")}).on("mouseleave",function(){$(this).children().addClass("animated")}),$("#toggleNav").on("click",o=>{o.preventDefault(),$("nav").toggleClass("active")}),$("nav ul a").on("click",function(){$("nav, nav a").removeClass("active"),$(this).addClass("active")}),window.onscroll=function(){!function(){let o=[{id:"#nav-home",position:-50},{id:"#nav-work",position:document.getElementById("work").offsetTop-200},{id:"#nav-cont",position:document.getElementById("contact").offsetTop-200}];$("nav, nav a").removeClass("active");for(let n=o.length;n>0;n--){let t=o[n-1];if(document.body.scrollTop>t.position||document.documentElement.scrollTop>t.position){$(t.id).addClass("active");break}}}()}});
+$(document).ready(function(){
+	
+	loadUser();
+	function loadUser() {
+		$.get( "js/work.json", function( data ) {
+			var template = $('#template').html();
+			Mustache.parse(template);   // optional, speeds up future uses
+			var rendered = Mustache.render(template, data);
+			$('#projects-target').html(rendered);
+		}, "json" );
+	}
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+
+	//Stop infinite animation on hover
+	$(".infinite-toggle").on('mouseenter', function(){
+		$(this).children().removeClass("animated");
+	}).on('mouseleave', function(){
+		$(this).children().addClass("animated");
+	})
+
+	//Toggle Nav on mobile
+	$("#toggleNav").on('click', (ev) => {
+		ev.preventDefault();
+		$('nav').toggleClass("active");
+	})
+	$('nav ul a').on('click', function(){
+		$('nav, nav a').removeClass("active");
+		$(this).addClass("active");
+	})
+
+	// When the user scrolls down 50px from the top of the document, resize the header's font size
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		let sections = [
+			{	'id' : '#nav-home', 'position' : -50 	},
+			{	'id' : '#nav-work', 'position' : document.getElementById("work").offsetTop - 200 	},
+			{	'id' : '#nav-cont', 'position' : document.getElementById("contact").offsetTop -200 	}
+		]
+		
+		$('nav, nav a').removeClass("active");
+		for (let i = sections.length; i > 0; i--) {
+			let section = sections[i-1];
+			if (document.body.scrollTop > section.position || document.documentElement.scrollTop > section.position) {
+				$(section.id).addClass("active");
+				break;
+			}
+		}
+		/* if (document.body.scrollTop > positionContact || document.documentElement.scrollTop > positionContact) {
+			$('#nav-cont').addClass("active");
+		} else if (document.body.scrollTop > positionWork || document.documentElement.scrollTop > positionWork) {
+			$('#nav-work').addClass("active");
+		} else if (document.body.scrollTop > positionHome || document.documentElement.scrollTop > positionHome) {
+			$('#nav-home').addClass("active");
+		} */
+	}
+});
